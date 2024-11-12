cf6e9f0b1bbf15eca98d420fab6544ca23100d3d
Jamie McCallum <jamiemccallum0@gmail.com>
2024-11-12 21:29:01 +0000

    updating with scrimba projects

---
+++ counter-app/index.js
@@ -0,0 +1,21 @@
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let incrementBtn = document.getElementById("increment-btn");
+let saveBtn = document.getElementById("save-btn");
+let count = 0;
+
+incrementBtn.addEventListener("click", increment);
+
+saveBtn.addEventListener("click", save);
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+}
+
+function save() {
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
+}
